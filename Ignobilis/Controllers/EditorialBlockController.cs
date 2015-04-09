993de05f0b8eb017428ca21032465a57a9cd64a1
xoni <johan.erik.obrink@gmail.se>
2015-04-09 13:20:44 +0200

    Ett litet editorblock

---
+++ Ignobilis/Controllers/EditorialBlockController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+using EPiServer.Web.Mvc;
+using Ignobilis.Models.Blocks;
+
+namespace Ignobilis.Controllers
+{
+    public class EditorialBlockController : BlockController<IB_EditorialBlock>
+    {
+        public override ActionResult Index(IB_EditorialBlock currentBlock)
+        {
+            return PartialView("~/Views/Ignobilis/Blocks/Editorial/index.cshtml", currentBlock);
+        }
+    }
+}
---
+++ Ignobilis/Models/Blocks/IB_EditorialBlock.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace Ignobilis.Models.Blocks
+{
+    [ContentType(DisplayName = "IB_PuffBlock", GUID = "df82ab6e-6123-40d9-a45f-ad8cf64dd970", Description = "")]
+    public class IB_EditorialBlock : IB_BaseBlock
+    {        
+        [CultureSpecific]
+        [Display(
+            Name = "Huvudinnehåll",
+            Description = "Text på blocket som visas som en information.",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]        
+        public virtual XhtmlString MainBody { get; set; }
+    }
+}
