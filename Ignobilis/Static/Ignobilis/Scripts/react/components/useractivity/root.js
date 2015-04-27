a1933fcfdaf18211d5cb74b456d1694d1258473f
xonium <johan.erik.obrink@gmail.com>
2015-04-27 13:54:53 +0200

    ooops. denna måste med

---
+++ Ignobilis/Static/Ignobilis/Scripts/react/components/useractivity/root.js
@@ -0,0 +1,13 @@
+﻿import React from 'react';
+
+export default class UserActivityRoot extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render(){
+        return(
+            <span>{this.props.users.TotalNumberOfConnections}</span>
+            );
+    }
+}
