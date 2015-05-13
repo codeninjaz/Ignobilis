a096582a9d86004d1d631390c64f60cde6850a25
xonium <johan.erik.obrink@gmail.com>
2015-05-13 12:10:42 +0200

    kan ju vara bra om apit kommer med

---
+++ Ignobilis/Business/Api/FileController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace Ignobilis.Business.Api
+{
+    public class FileController : ApiController
+    {
+        [Route("api/jesus")]
+        public string GetAFuckingString()
+        {
+            return "JESUS";
+        }
+    }
+}
