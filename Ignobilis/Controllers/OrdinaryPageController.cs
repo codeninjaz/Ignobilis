7c90bbb8913ffceee2a7cedc8afc7f15997e557a
xonium <johan.erik.obrink@gmail.com>
2015-05-27 16:26:38 +0200

    liten fix för menyn

--- Ignobilis/Controllers/OrdinaryPageController.cs
+++ Ignobilis/Controllers/OrdinaryPageController.cs
@@ -8,7 +8,7 @@ namespace Ignobilis.Controllers
     public class OrdinaryPageController : PageController<IB_OrdinaryPage>
     {
         public ActionResult Index(IB_OrdinaryPage currentPage)
-        {            
+        {                        
             var pageViewModel = new PageViewModel<IB_OrdinaryPage>(currentPage, User);
             return View("~/Views/Ignobilis/Pages/OrdinaryPage/Index.cshtml", pageViewModel);
         }
